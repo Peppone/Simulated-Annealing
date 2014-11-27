9c0eb27336e210f69bd22dd9df1ba84db004a1e6
TheDreadnought <TheDreadnought@users.noreply.github.com>
2014-11-27 17:55:11 +0100

    Test SATabu MO

---
+++ test/TestSATabuMO.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.ArrayList;
+
+import solution.Solution;
+import taboo.SimulatedAnnealingTabuMO;
+import acceptance.AcceptancePolicy;
+import acceptance.DefaultMOAcceptance;
+import aspiration.DefaultMOAspiration;
+import cooling.CoolingProcedure;
+import cooling.DecrementalCooling;
+
+public class TestSATabuMO extends SimulatedAnnealingTabuMO
+{
+	public TestSATabuMO(Path start, double initialTemperature,
+			CoolingProcedure cp, AcceptancePolicy ap) {
+		super(start, initialTemperature, cp, ap, 10, new DefaultMOAspiration());
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) 
+	{
+		ArrayList<Solution> solutions;
+		TestSATabuMO test=new TestSATabuMO(new Path(), 100,new DecrementalCooling(),new DefaultMOAcceptance());
+		solutions=test.execute();
+		System.out.println("Solutions ("+solutions.size()+"): ");
+		for(Solution p:solutions)
+			System.out.println((Path)p);
+	}
+}
