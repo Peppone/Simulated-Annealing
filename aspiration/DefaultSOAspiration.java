c5d3b169f33e599bdb57472659bc12d6aa190eda
TheDreadnought <TheDreadnought@users.noreply.github.com>
2014-11-27 17:28:21 +0100

    New default aspiration

--- aspiration/DefaultSOAspiration.java
+++ aspiration/DefaultSOAspiration.java
@@ -2,13 +2,13 @@ package aspiration;
 
 import sa.SimulatedAnnealing;
 
-
-public class DefaultSOAspiration implements AspirationCriterion {
-
+public class DefaultSOAspiration implements AspirationCriterion
+{
 	@Override
 	public double aspirationCriterion(SimulatedAnnealing sa) 
 	{
-		return 0.5;
+		double diff=(Double)sa.getActualSolution().fitness()[0]-(Double)sa.getNeighbour().fitness()[0];
+		diff= diff>0?-diff:diff;
+		return Math.exp( diff/sa.getTemperature());
 	}
-
 }
